5196337b7926e05e6037d4c905ddf8132ff7773e
andrey.grushevskiy <grushevskiy@gmail.com>
2019-01-22 16:35:49 +0000

    completed html

---
+++ main.js
@@ -0,0 +1,26 @@
+var allQuestions = [
+    { 
+        question: "Who is Prime Minister of the United Kingdom?", 
+        choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+        correctAnswer: 0 
+    },
+    {
+        question: "What is your favourite colour?",
+        choices: ["Green", "Brown", "Blue", "Red"],
+        correctAnswer: 3
+    },
+    {
+        question: "Who is your name?",
+        choices: ["Bob", "Paul", "Andrey", "Alex"],
+        correctAnswer: 2
+    },
+];
+
+
+function calculate(){
+    document.querySelector()
+}
+
+document.querySelector("#calculate").addEventListener('click', function (){
+    calculate();
+})
